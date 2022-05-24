e015bbadda57a5d64ac5cf2ca16e263689ad97f0
Alex Córdova <alexcordova111@gmail.com>
2022-05-23 19:38:01 -0500

    variables

---
+++ day-two/1-practica-variables/practica.py
@@ -0,0 +1,9 @@
+nombre = "Tony Soprano"
+edad = 51
+
+nombre = "Julia"
+apellido = "Roberts"
+nombrecompleto = nombre + " " + apellido
+
+curso = "Python"
+print("Estás tomando un curso de " + curso)
