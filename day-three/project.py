0f998ae702fce7ac0f8cf1f8ac0720a98d895a21
Alex Córdova <alexcordova111@gmail.com>
2022-07-16 19:09:22 -0500

    Python project

--- day-three/project.py
+++ day-three/project.py
@@ -9,10 +9,6 @@ for letter in letters:
     n_letters = texts.count(letter.strip().lower())
     count_letters[letter] = n_letters
 
-texto_invertido = ' '.join(text_list[::-1])
-
-
-################################################################
 print(f"El número de palabras del texto es {n_palabras}")
 print("Numero de veces por cada letra:")
 for key, value in count_letters.items():
@@ -20,5 +16,5 @@ for key, value in count_letters.items():
 
 print(f"Primera letra del texto: {texts[0]}")
 print(f"Última letra del texto: {texts[-1]}")
-print(f"Texto invertido: {texto_invertido}")
+print(f"Texto invertido: {' '.join(text_list[::-1])}")
 print(f"La palabra 'Python' esta dentro del texto?: {'python' in text_list}")
