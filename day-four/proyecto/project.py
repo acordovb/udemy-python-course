446c9a364c3096e0c9ef86e1fbbb6033bcc20f05
Alex Cordova <alexcordova111@gmail.com>
2022-08-22 23:40:28 -0500

    Proyecto del dia 4

---
+++ day-four/proyecto/project.py
@@ -0,0 +1,26 @@
+from ast import match_case
+from random import random
+
+
+from random import randint
+n_random = randint(1, 20)
+intentos = 8
+name = input('Ingresa tu nombre: ')
+print(f'Hola {name}, he pensado un numero entre el 1 y el 100 y tienes solo {intentos} para adivinarlo. \n\
+Cual crees que es el numero?')
+numero = 0
+while intentos>0:
+    numero = int(input('Ingresa un numero: '))
+    if numero < 1 or numero > 100:
+        print('El numero no esta permitido dado que es mayor a 100 o menor que 1')
+    elif numero < n_random:
+        print('Tu respuesta es incorrecta, has elegido un numero menor al numero secreto.')
+    elif numero > n_random:
+        print('Tu respuesta es incorrecta, has elegido un numero mayor al numero secreto.')
+    elif numero==numero:
+        break
+    intentos -=1
+if numero == n_random:
+    print(f'Has ganado, te ha tomado {8-intentos} intentos')
+else:
+    print('PERDISTE')
